7d1bb66c92cfbb8f43d640974873da2631243832
Michael Penn <mwpenn94@gmail.com>
2020-12-10 23:51:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,20 @@ end
 def player_move(board, index, marker)
  board[index] = marker
 end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
+
